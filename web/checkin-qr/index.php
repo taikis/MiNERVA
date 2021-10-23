a8f209d7f251a2e95665d9c50cc2eb022c4da9c5
taiki <turkey.31452@gmail.com>
2021-10-23 17:05:25 +0900

    タイトル修正

--- web/checkin-qr/index.php
+++ web/checkin-qr/index.php
@@ -10,7 +10,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <title>Minerva</title>
+        <title>MiNERVA</title>
         <link rel="stylesheet" href="./css/app.css" />
     </head>
     <body>
